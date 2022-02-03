32b064bca14d02aea0da32ccfdd430beb7bdc531
Fariba <fariba.ghorbani2127@gmail.com>
2022-02-03 10:32:32 +0330

    HW13 is uploaded

--- 1/main.js
+++ 1/main.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
+
+    // change the id and name of radio button groups in different rows
     let countId = 4
     let countRowRadioOptions = 2
 
-    function makeCopy(element, countId, countRowRadioOptions) {
+    function makeCopy(element) {
         element.find(".form-check-input1").attr("id", "flexRadioDefault" + countId)
         element.find(".form-check-input1").attr("name", "rowRadioOptions" + countRowRadioOptions)
         element.find(".form-check-label1").attr("for", "flexRadioDefault" + countId)
@@ -19,19 +21,46 @@ $(document).ready(function(){
         return element
     }
 
-
-    $(".c-add-row").click(() => {
-        let temp = makeCopy($(".c-row").last().clone(), countId ,countRowRadioOptions)
+    // add an empty row
+    $(document).on('click', '.c-add-row', function(){
+        let temp = makeCopy($(".c-row").last().clone())
+        temp.find(".input-box").val("")
+        temp.find("input:radio").prop('checked', false)
+        temp.find(".input-box").prop("disabled", false)
         temp.prependTo("tbody")
-    })
+        numberHandling()
+    });
 
+    // delete a row
     $(document).on('click', '.c-delete', function(){
         $(this).parents().eq(1).remove()
+        numberHandling()
     });
 
+    // clone a row
     $(document).on('click', '.c-clone', function(){
-        let clonedElement = makeCopy($(this).parents().eq(1).clone(), countId, countRowRadioOptions)
+        let clonedElement = makeCopy($(this).parents().eq(1).clone())
         $(this).parents().eq(1).after(clonedElement)
+        numberHandling()
     });
 
+    
+    // enable/disable input box based o radio button value
+    $(document).on('click', '.form-check-input', function(){
+        let radioValue = $(this).val()
+        if (radioValue === "confirmed") {
+            $(this).parents().eq(2).find(".input-box").prop("disabled", true)
+        } else {
+            $(this).parents().eq(2).find(".input-box").prop("disabled", false)
+        }
+        numberHandling()
+    });
+
+    // change the number information below the table
+    function numberHandling() {
+        $(".all-rows span")
+        .text($(".tbody").children().length)
+        $(".not-confirmed-rows span")
+        .text($(".form-check-input[value=confirmed]:checked").length)
+    }
 });
